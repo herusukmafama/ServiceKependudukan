b6ac18e2e5c5dfa84e8d0c040f6946e51fa8e2f6
herusukma <herusukmafama@gmail.com>
2024-03-13 10:26:31 +0700

    Fixed CORS API

--- KependudukanAPI/obj/Debug/net5.0/KependudukanAPI.AssemblyInfo.cs
+++ KependudukanAPI/obj/Debug/net5.0/KependudukanAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KependudukanAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3597a2ff5837a94695d8bdf62925d1b228f27453")]
 [assembly: System.Reflection.AssemblyProductAttribute("KependudukanAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KependudukanAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
